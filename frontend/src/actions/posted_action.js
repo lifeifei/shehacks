588052395f9f9566bed85d39c4a2eb630a96dc57
Lifei Zhou <lifeiz@gmail.com>
2017-07-21 14:18:18 +1000

    used the correct url

--- frontend/src/actions/posted_action.js
+++ frontend/src/actions/posted_action.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const SEND_EMAIL_ADDRESS_SUCCESS = 'SEND_EMAIL_ADDRESS_SUCCESS';
 
 export const sendEmailAddress = () => {
-    let url = 'https://g40zteabni.execute-api.ap-southeast-2.amazonaws.com/beta/post-emai';
+    let url = 'https://g40zteabni.execute-api.ap-southeast-2.amazonaws.com/beta/post-email';
     return (dispatch, getState) => {
         axios.post(url, {
             email: getState().form.posted.values.email
